04bf7cf2b886606e4326d9f0aa33467e7317aab8
Rupinder Sharma <156651557+rupinder1274@users.noreply.github.com>
2025-02-08 16:31:12 +0530

    Update passport.js

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -1,1 +1,44 @@
+require("dotenv").config();
 
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const passport = require("passport");
+
+const User = require("../models/user.model");
+
+const { v4: uuidv4 } = require("uuid");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `https://${process.env.BACKEND_URL}/auth/google/callback`,
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      const email = profile?._json?.email;
+      const name = profile?._json?.name;
+      const picture = profile?._json?.picture;
+      let user;
+      try {
+        user = await User.findOne({ email }).lean().exec();
+
+        if (!user) {
+          user = await User.create({
+            email: email,
+            password: uuidv4(),
+            name: name,
+            profilePic: picture,
+            donations: [],
+          });
+        }
+
+        return cb(null, user);
+      } catch (err) {
+        console.log({ err });
+      }
+    }
+  )
+);
+
+module.exports = passport;
