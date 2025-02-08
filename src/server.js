e5991cefd518ef11e9e8aeedc6b92ce9b70fa01e
Rupinder Sharma <156651557+rupinder1274@users.noreply.github.com>
2025-02-08 16:29:31 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
+require("dotenv").config();
 
+const app = require("./index");
+const connect = require("./configs/db");
+
+const PORT = process.env.PORT || 9900;
+
+app.listen(PORT, async () => {
+    console.log("starting");
+    await connect();
+    console.log("listening to port", PORT);
+})
