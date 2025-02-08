5fba03da74b45457db976957e2c275321d222128
Rupinder Sharma <156651557+rupinder1274@users.noreply.github.com>
2025-02-08 16:35:27 +0530

    Update NGO.model.js

--- src/model/NGO.model.js
+++ src/model/NGO.model.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const NGOSchema = new mongoose.Schema({
+    NGOName: {type: String, required: true},
+    reviews: {type: Number, required: true},
+    totalCampaigns: {type: Number, required: true},
+    totalFeeds: {type: Number, required: true},
+    about: {type: String, required: true},
+    image: {type: String, required: true},
+    needFood: {type: Boolean, required: true},
+    totalVolunteers: {type: Number, required: true}
+})
+
+module.exports = mongoose.model("ngos", NGOSchema);
