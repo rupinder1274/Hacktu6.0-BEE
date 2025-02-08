2cda94a7e8573892709f64d24a4747cd67eb2c37
Rupinder Sharma <156651557+rupinder1274@users.noreply.github.com>
2025-02-08 16:30:32 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+module.exports = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ez6pn.mongodb.net/Seva?retryWrites=true&w=majority`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
